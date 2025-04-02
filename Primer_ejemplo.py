040cd28a0c7342b1974e741a8c36fffe2ae74d78
andrecalmus <A01733529@tec.mx>
2025-04-01 18:38:56 -0600

    Agregar for y mover datos

--- Primer_ejemplo.py
+++ Primer_ejemplo.py
@@ -1,4 +1,9 @@
-print("Hola")
+print("Hola a todos, este es un repositorio local y se deslpegará en GitHub")
+
+print("Este archivo se está modificando")
+
+for i in range(10):
+    print(i)
 
 def suma(a, b):
     return a + b
